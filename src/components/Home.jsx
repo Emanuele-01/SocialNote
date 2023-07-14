126e826654f1b3680c01baa3e453ce14f459537f
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-07-14 16:02:18 +0200

    delete post update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,6 @@ const Home = () => {
 
     const dispatch = useDispatch()
     const [allPost, setAllPost] = useState([])
-    const [singlePost, setSinglePost] = useState()
 
     const access = useSelector(state => state.token.content);
 
@@ -41,11 +40,9 @@ const Home = () => {
         dispatch(getAllPost())
     }, [dispatch])
 
-    const handleDeletePost = async (event) => {
-        event.preventDefault();
-        setSinglePost(this.post.id)
-        dispatch(deletePost(singlePost, access.accessToken))
-    }
+    const handleDeletePost = async (postId) => {
+        dispatch(deletePost(postId, access.accessToken));
+    };
     return (
         <Row className="justify-content-center">
             <Col xs={12}>
@@ -67,7 +64,7 @@ const Home = () => {
                                     id="dropdown-menu-align-end"
                                     className=""
                                 >
-                                    <Dropdown.Item eventKey="1" className="ms-3" onClick={handleDeletePost}>
+                                    <Dropdown.Item eventKey="1" className="ms-3" onClick={() => handleDeletePost(post.id)}>
                                         {<svg xmlns="http://www.w3.org/2000/svg" className="me-3 mb-1" width="18" height="18" fill="#c80d0d" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                                         </svg>} Delete
